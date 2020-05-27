d6a3a1452fcb44378d1b9afaaa7a09e4cc710049
Zach Vander Velden <zachvv11@gmail.com>
2020-05-26 21:16:50 -0500

    pipeline verification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Coming Soon!!
+                    Coming Soon!!!
                 </div>
 
                 <div class="links">
