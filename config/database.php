b59a7f9c3efc9709c4630003c564e22227dd1b73
Zach Vander Velden <zachvv11@gmail.com>
2020-05-29 00:16:08 -0500

    removing cluster mode

--- config/database.php
+++ config/database.php
@@ -142,16 +142,6 @@ return [
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
-        'clusters' => [
-            'default' => [
-                [
-                    'host' => env('REDIS_HOST', 'localhost'),
-                    'password' => env('REDIS_PASSWORD', null),
-                    'port' => env('REDIS_PORT', 6379),
-                    'database' => 0,
-                ],
-            ],
-        ],
     ],
 
 ];
