36b418ae425ccb11ffdc59c6c93ee161013c7530
Zach Vander Velden <zachvv11@gmail.com>
2020-05-29 01:06:18 -0500

    forcing debug mode on. elastic beanstalk is tricky when updating config in console.

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => true,//(bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
